a486e07282bf3dd134c296f2abd2ce8e356ba3a9
Chris Maguire <cwmaguire@protonmail.com>
2025-06-14 12:31:19 -0700

    Fix datebox characters, clean up
    Goose/xAI or Gemini used pipes and dashes for the box but I wanted nicer
    lines. I'm not sure if they're ascii.
    
    Also cleaned up some old comments (they're preserved in git comments and
    diffs) and simplified the Lua a bit.
    
    I should probably give the AIs more instructions on the style of code I
    like.

--- lua/datebox/init.lua
+++ lua/datebox/init.lua
@@ -2,25 +2,30 @@
 -- "Please rewrite this plugin so that instead of printing out the datebox
 --  in the message area, it inserts the datebox as text at the cursor"
 
+┏━━━━━━━━━━━━━━━━┓
+┃   2025-06-14   ┃
+┗━━━━━━━━━━━━━━━━┛
+
+
 local M = {}
 
 function M.show_datebox()
-    -- Get current date
     local date = os.date("%Y-%m-%d")
 
     -- Calculate dimensions for the box
     local date_length = #date
-    local padding = 2 -- spaces on each side of the date
-    local box_width = date_length + (padding * 2) + 2 -- Date + padding + borders (| |)
-
-    local horizontal_line = "+" .. string.rep("-", box_width - 2) .. "+"
-    local content_line = "|" .. string.rep(" ", padding) .. date .. string.rep(" ", padding) .. "|"
+    local padding = "  "
+    local box_width = date_length + (#padding * 2)
+    local line = string.rep("━", box.width)
+    local top_line = "┏" .. line .. "┓"
+    local content_line = "┃" .. padding .. date .. padding .. "┃"
+    local bottom_line = "┗" .. line .. "┛"
 
     -- Prepare the lines to insert
     local lines_to_insert = {
-        horizontal_line,
+        top_line,
         content_line,
-        horizontal_line
+        bottom_line
     }
 
     -- Get current buffer and cursor position
@@ -28,39 +33,16 @@ function M.show_datebox()
     local cursor_row = vim.api.nvim_win_get_cursor(0)[1] - 1 -- -1 because nvim_buf_set_lines is 0-indexed
 
     -- Insert the lines at the cursor position
-    -- The third argument 'true' means 'strict_indexing' (start and end are exclusive for removal)
+    -- 'strict_indexing': start and end are exclusive for removal
     -- Here we want to insert, so start and end are the same, effectively inserting before that line.
-    vim.api.nvim_buf_set_lines(current_buf, cursor_row, cursor_row, false, lines_to_insert)
+    local strict_indexing = false
+    local start = cursor_row
+    local end = cursor_row
+    vim.api.nvim_buf_set_lines(current_buf, start, end, strict_indexing, lines_to_insert)
 
     -- Optionally, move the cursor to the middle of the inserted box (on the date line)
     -- This calculation places the cursor at the beginning of the date string
     vim.api.nvim_win_set_cursor(0, {cursor_row + 2, padding + 1}) -- +2 for the middle line, +1 for initial |
 end
 
--- Expose the function as a Neovim command
--- (CM: According to Gemini this needs to be in a file auto-loaded by Lazy.nvim,
---  which means it needs to be in the plugin directory)
---vim.api.nvim_create_user_command('DateBox', M.show_datebox, {})
-
--- Simple datebox plugin for Neovim
 return M
-
--- Original by xAI (I didn't give a good prompt)
--- local M = {}
--- 
--- function M.show_datebox()
---     local date = os.date("%Y-%m-%d")
---     local line_length = #date + 4 -- Date length + 2 for padding + 2 for borders
---     local horizontal_line = "+" .. string.rep("-", line_length - 2) .. "+"
---     local content_line = "|  " .. date .. "  |"
--- 
---     print(horizontal_line)
---     print(content_line)
---     print(horizontal_line)
--- end
--- 
--- -- Expose the function as a Neovim command
--- vim.api.nvim_create_user_command('DateBox', M.show_datebox, {})
--- 
--- -- Simple datebox plugin for Neovim
--- return M
